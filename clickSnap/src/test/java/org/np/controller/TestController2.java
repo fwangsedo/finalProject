483386c882122f787b420ce418f1f4f43aab88b1
baekhw0729 <baekhw0729@naver.com>
2021-05-31 19:41:30 +0900

    테스트컨트롤러야 올라가거라

---
+++ clickSnap/src/test/java/org/np/controller/TestController2.java
@@ -0,0 +1,13 @@
+package org.np.controller;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class TestController2 {
+	@Test
+	public void testDelete() {
+		 log.info("DELETE COUNT : ");
+	 }
+}
