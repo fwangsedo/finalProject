c61bb5332cf41f8532c94a82b400b5167f8e9dd6
fwangsedo <tpehek@naver.com>
2021-05-31 19:03:47 +0900

    test로만들어보았습니다

---
+++ clickSnap/src/test/java/org/np/controller/TestController.java
@@ -0,0 +1,14 @@
+package org.np.controller;
+
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class TestController {
+	@Test
+	public void testDelete() {
+		 log.info("DELETE COUNT : ");
+	 }
+}
